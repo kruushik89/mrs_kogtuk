d82373fa09804b2087f3c4113d5f073bde9f13a2
kruushik89 <kruushik89@gmail.com>
2023-04-26 23:13:41 +0300

    finish logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,10 +7,14 @@ import logoImage from './logo.png';
 import FacebookIcon from '@/public/svg/Facebook';
 import InstagramIcon from '@/public/svg/Instagram';
 import TelegramIcon from '@/public/svg/Telegram';
+import { HeaderProps } from '@/components/Header/header.props';
+import classNames from 'classnames';
 
-const Header = () => {
+const Header: React.FC<HeaderProps> = ({ scrollTop }) => {
   return (
-    <div className={styles.header}>
+    <div className={classNames(styles.header, {
+      [styles.bgBlack]: scrollTop >= 100
+    })}>
       <div className={styles.headerWrap}>
         <nav className={styles.navigation}>
           <div className={styles.navigationItem}>Прайс-лист</div>
@@ -18,13 +22,15 @@ const Header = () => {
           <div className={styles.navigationItem}>Галерея</div>
           <div className={styles.navigationItem}>Контакти</div>
         </nav>
-        <div className={styles.logo}>
-          <Image src={logoImage} alt="Logo" width={50} height={50}/>
-        </div>
+        {scrollTop >= 100 && (
+          <div className={styles.logo}>
+            <Image src={logoImage} alt="Logo" width={50} height={50}/>
+          </div>
+        )}
         <div className={styles.socialIcons}>
-          <a href="https://t.me/l_a_andreevna"><TelegramIcon /></a>
-          <a href="https://instagram.com/mrs.kogtuk?igshid=YmMyMTA2M2Y="><InstagramIcon /></a>
-          <a href="https://www.facebook.com/profile.php?id=100010697550526"> <FacebookIcon /></a>
+          <a href="https://t.me/l_a_andreevna"><TelegramIcon/></a>
+          <a href="https://instagram.com/mrs.kogtuk?igshid=YmMyMTA2M2Y="><InstagramIcon/></a>
+          <a href="https://www.facebook.com/profile.php?id=100010697550526"> <FacebookIcon/></a>
         </div>
       </div>
     </div>
--- components/MainScreen/MainScreen.tsx
+++ components/MainScreen/MainScreen.tsx
@@ -6,13 +6,16 @@ import sterileImage from '@/public/images/Steryl.png';
 import comfortImage from '@/public/images/Comfortable.png';
 import bigLogoImage from '@/public/images/big-logo.png';
 import AdvantageItem from '@/components/Advantages/AdvantageItem';
+import { MainProps } from '@/components/MainScreen/main.props';
 
-const MainScreen = (): JSX.Element => {
-  return(
+const MainScreen: React.FC<MainProps> = ({ scrollTop }): JSX.Element => {
+  return (
     <div className={styles.main}>
-      <div className={styles.bigLogo}>
-        <img src={bigLogoImage.src} alt="Big logo"/>
-      </div>
+      {scrollTop < 100 && (
+        <div className={styles.bigLogo}>
+          <img src={bigLogoImage.src} alt="Big logo"/>
+        </div>
+      )}
       <div className={styles.mainWrap}>
         <ul className={styles.description}>
           <li className={styles.descriptionItem}>Індивідуальний підхід</li>
@@ -20,9 +23,9 @@ const MainScreen = (): JSX.Element => {
           <li className={styles.descriptionItem}>Обожнюю малювати френч</li>
         </ul>
         <div className={styles.advantages}>
-          <AdvantageItem label='Безпечно'  style='safely' alt='Safely image' {...safetyImage} />
-          <AdvantageItem label='Стерильно'  style='sterile' alt='Sterile image' {...sterileImage} />
-          <AdvantageItem label='Комфортно'  style='comfort' alt='Comfort image' {...comfortImage} />
+          <AdvantageItem label="Безпечно" style="safely" alt="Safely image" {...safetyImage} />
+          <AdvantageItem label="Стерильно" style="sterile" alt="Sterile image" {...sterileImage} />
+          <AdvantageItem label="Комфортно" style="comfort" alt="Comfort image" {...comfortImage} />
         </div>
       </div>
     </div>
